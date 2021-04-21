a44ed1088c3b5ed5c0723358d534b3dbfd42b6cc
Anshul Kaushal <anshul.kaushal1997@gmail.com>
2021-04-21 23:20:22 +0530

    ran tests to ensure recipes can be added and listed

--- test/cookbook.test.js
+++ test/cookbook.test.js
@@ -3,13 +3,24 @@ const { Cookbook } = require('../src/cookbook');
 describe('Cookbook', () => {
   describe('Adding recipes', () => {
     test('should allow a recipe to be added', () => {
+      const myCookbook = new Cookbook();
+      myCookbook.addRecipe('mango shake', ['mango', 'milk']);
 
+      const expectedRecipe = {'mango shake': ['mango', 'milk']};
+
+      expect(myCookbook.recipes).toEqual(expectedRecipe);
     });
   });
 
   describe('Listing recipes', () => {
     test('should allow the recipes to be listed', () => {
+      const myCookbook = new Cookbook();
+      myCookbook.addRecipe('mango shake', ['mango', 'milk']);
+      myCookbook.addRecipe('burger', ['buns', 'patty']);
 
+      const expectedRecipesList = ['mango shake', 'burger'];
+
+      expect(myCookbook.listRecipes()).toEqual(expectedRecipesList);
     });
   });
 
