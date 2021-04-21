27cc0e9a827285bc613b0aba420d3419f6e0ffd8
Anshul Kaushal <anshul.kaushal1997@gmail.com>
2021-04-22 00:35:12 +0530

    ran tests to verify the recipes list displayed and the ingredients returned to make a specified recipe

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -16,13 +16,29 @@ describe('CookbookCli', () => {
 
   describe('Listing recipes', () => {
     test('should display the correct message listing all of the recipe names', () => {
+      const myCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myCookbook);
+      myCookbookCli.run('add', 'pepperoni pizza', ['pepperoni', 'pizza']);
+      myCookbookCli.run('add', 'cookies', ['butter', 'dough']);
 
+      const message = myCookbookCli.run('list');
+      const expectedMessage = `You have the following recipes: pepperoni pizza,cookies`;
+
+      expect(message).toBe(expectedMessage);
     });
   });
 
   describe('Retrieving a recipe', () => {
     test('should display the ingredients required to make the specified recipe', () => {
+      const myCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myCookbook);
 
+      myCookbookCli.run('add', 'chips', ['potatoes', 'salt']);
+
+      const message = myCookbookCli.run('get', 'chips');
+      const expectedMessage = `The ingredients for chips are: potatoes,salt`;
+
+      expect(message).toBe(expectedMessage);
     });
   });
 
