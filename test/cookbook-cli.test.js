44bf814c1cd278c5cc8e1c8d6f6b295213c75eff
Anshul Kaushal <anshul.kaushal1997@gmail.com>
2021-04-21 23:55:49 +0530

    ran test to verify the message returned by cookbook cli on adding a new recipe

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -4,7 +4,13 @@ const { CookbookCli } = require('../src/cookbook-cli');
 describe('CookbookCli', () => {
   describe('Adding recipes', () => {
     test('should accept the recipe information and display the correct message', () => {
+      const myCookbook = new Cookbook();
+      const myCookbookCli = new CookbookCli(myCookbook);
 
+      const message = myCookbookCli.run('add', 'pepperoni pizza', ['pepperoni', 'pizza']);
+      const expectedMessage = `Successfully added the following recipe: pepperoni pizza`;
+
+      expect(message).toBe(expectedMessage);
     });
   });
 
